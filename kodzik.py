c3d3de5aef8ec4ef7072b88a817ee466fac168d1
Igiel132 <90388632+Igiel132@users.noreply.github.com>
2021-09-29 11:59:12 +0200

    Add files via upload

---
+++ kodzik.py
@@ -0,0 +1,19 @@
+#pesel = int(input("Podaj pesel: "))
+#x = pesel // 100
+#x *= 100
+#y = pesel - x #wyciąga dwie ostatnie liczby pod y
+#z = y // 10
+#print(z)
+#if (z % 2 == 0 ):
+#    print("Jesteś kobietą!")
+#else:
+#    print("Jesteś mężczyzną")
+
+x = int(input("Podaj liczbę punktów zdobytych przez klasę: "))
+y = float(input("Podaj średnią frekwencje: "))
+z = float(input("Podaj średnią klasy: "))
+if(y > 94 and z > 5):
+    x += 20
+    print("Klasa posiada", x, "punktów!")
+else:
+    print("Klasa posiada", x, "punktów!")
